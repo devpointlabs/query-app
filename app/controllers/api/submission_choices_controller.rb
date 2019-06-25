2a6cd240b4cef23dc2c30bc511a89c6f3e1c24ff
pompucha <benavidesga@gmail.com>
2019-06-25 11:42:18 -0600

    built submission choice controller

--- app/controllers/api/submission_choices_controller.rb
+++ app/controllers/api/submission_choices_controller.rb
@@ -1,13 +1,24 @@
 class Api::SubmissionChoicesController < ApplicationController
+  before_action :set_submission
+  before_action :set_choice
+  before_action :set_submission_choice, only: [:index]
+
   def index
+    render json: @submission.choice.submission_choices  
   end
 
-  def show
+  private 
+
+  def set_submission_choice
+    @submission_choice = Submission_choice.find(params[:id])
   end
 
-  def update
+  def set_submission
+    @submission = Submission.find(params[:submission_id])
   end
 
-  def create
+  def set_choice
+    @choice = Choice.find(params[:choice_id])
   end
+
 end
