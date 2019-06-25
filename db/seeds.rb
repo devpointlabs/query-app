7cafa4ef28582973f26fe7316c18089b3fece16a
tallan groberg <tallan.taven@gmail.com>
2019-06-25 15:47:43 -0600

    add seeds file for users

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+User.create(
+  email: 'test@test.com',
+  password: 'password'
+)
+
+99.times do
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    nickname: Faker::Creature::Cat.name,
+    password: "password",
+    image: Faker::Avatar.image("name", '100x400', 'png', 'set4')
+  )
+end
+
+puts "100 Users Seeded"
