c16194afe1e69e07eb056dbd55c01c1051e0645e
tallan groberg <tallan.taven@gmail.com>
2019-06-26 15:48:38 -0600

    quizzes controller complete

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -2,6 +2,7 @@ class Api::QuizzesController < ApplicationController
   before_action :set_quiz, only: [:show, :update, :create, :destroy]
 
   def index
+
     render json: Quiz.all
 
   end
@@ -11,19 +12,33 @@ class Api::QuizzesController < ApplicationController
   def create
     quiz = Quiz.new(quiz_params)
 
+    if quiz.save
+      render json:quiz
+    else
+      render json: { errors: quiz.errors }, status: :unprocessable_entity
 
+    end
   end
   def update
+    quiz = Quiz.find(params[:id])
+    if quiz.update
+      render json: quiz
+    else
+      render json: { errors: quiz.errors }, status: :unprocessable_entity
+    end
 
   end
   def destroy
+    #could break here
 
+    @quiz.destroy
   end
   private
   def set_quiz
+    #could break here
     @quiz = Quiz.find(params[:id])
   end
   def quiz_params
-    params.require(:quiz).permit(:name)
+    params.require(:quizzes).permit(:name)
   end
 end
