8dedf96c76505aa66b05c1af56e3f2beb86621a6
tallan groberg <tallan.taven@gmail.com>
2019-06-26 15:16:03 -0600

    all basic controllers

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -1,2 +1,26 @@
 class Api::QuizzesController < ApplicationController
+  before_action :set_quiz, only: [:show, :update, :create, :destroy]
+
+  def index
+
+  end
+  def show
+
+  end
+  def create
+
+  end
+  def update
+
+  end
+  def destroy
+
+  end
+  private
+  def set_quiz
+
+  end
+  def quiz_params
+
+  end
 end
