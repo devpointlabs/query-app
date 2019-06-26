1a0d84c93892e85e9cd4fc9f1412dfc5a6db816b
tallan groberg <tallan.taven@gmail.com>
2019-06-26 15:31:07 -0600

    add faker gem and half quiz controller

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -2,12 +2,15 @@ class Api::QuizzesController < ApplicationController
   before_action :set_quiz, only: [:show, :update, :create, :destroy]
 
   def index
+    render json: Quiz.all
 
   end
   def show
 
   end
   def create
+    quiz = Quiz.new(quiz_params)
+
 
   end
   def update
@@ -18,9 +21,9 @@ class Api::QuizzesController < ApplicationController
   end
   private
   def set_quiz
-
+    @quiz = Quiz.find(params[:id])
   end
   def quiz_params
-
+    params.require(:quiz).permit(:name)
   end
 end
