9c885961247e00b50f1bf816f1ee7ad5944202bb
Serena Hong <serenahong.usa@gmail.com>
2019-07-01 20:28:56 -0600

    added view and delete on quiz

--- client/src/components/teacher/TeacherShowQuizzes.js
+++ client/src/components/teacher/TeacherShowQuizzes.js
@@ -1,57 +1,66 @@
 import React, {useState, useEffect, } from 'react';
-import { Container, Button, Card, Segment, Icon, } from 'semantic-ui-react';
+import { Container, Card, Segment, Button, Header, Icon, } from 'semantic-ui-react';
+import { Link, } from "react-router-dom";
 import axios from 'axios';
-import {Link} from 'react-router-dom';
-import QuizForm from './QuizForm'
-
-
 
 const TeacherShowQuizzes = (props) => {
-    
-    const [ quizzes, setQuizzes ] = useState([])
-    const [showForm, setShowForm] = useState(false);
+  const [ quizzes, setQuizzes ] = useState([])
 
-    useEffect( () => {
-        axios.get("/api/quizzes")
-          .then( res => {
-            setQuizzes(res.data);
-          })
-      }, [])
+  useEffect( () => {
+    axios.get("/api/quizzes")
+      .then( res => {
+        setQuizzes(res.data);
+    })
+  }, []);
 
-      const renderQuizzes = () => {
-        
-        return quizzes.map( quiz => (
-          <>
-          
-          <Container>
-            
-            <Segment key={quiz.id}>
-                <Card.Group>
-                <Card>
+  const deleteQuiz = (id) => {
+    axios.delete(`api/quizzes/${id}`)
+      .then(res => {
+        let values = (quizzes.filter( quiz => quiz.id !== id ))
+        setQuizzes(values)
+      });
+    renderQuizzes();
+  }
+
+  const renderQuizzes = () => {
+    if(quizzes.length <= 0)
+      return <Header as='h2'> - No Quizzes Available -</Header>
+    return quizzes.map( quiz => (
+      <>
+        <Container> 
+          <Segment key={quiz.id}>
+            <Card.Group>
+              <Card>
                 <Card.Content>
-                    <Card.Header> {quiz.name} </Card.Header>
-                    {/* <Card.Description> {quiz.description} </Card.Description> */}
+                  <Card.Header> {quiz.name} </Card.Header>
                 </Card.Content>
-                </Card>
-               
+                <Card.Content extra>
+                  <Button as={Link} to={`/quizzes/${quiz.id}`} class="ui violet basic button">  
+                    View
+                  </Button>
+                  <Button class="ui violet basic button" onClick={() => deleteQuiz(quiz.id)}> 
+                    <Icon name="trash"/> Delete
+                  </Button>
+                </Card.Content>
+              </Card>  
             </Card.Group>
-            </Segment>
-          </Container>
-          <br />
-          <br />
-          <br />
-
-          </>
-        ))
-      }
-    
-    
+          </Segment>
+        </Container>
+        <br />
+        <br />
+        <br />
+      </>
+    ))
+  }
     return (
-        <>
-
-    {renderQuizzes()}
-       
-        </>
+      <>
+        <br />
+        <br />
+        <br />
+        <Card.Group>
+          {renderQuizzes()}
+        </Card.Group>
+      </>
     )
 }
 
