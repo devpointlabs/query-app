501f2b9a8db5dcb9af96126365d941119af689fa
pompucha <benavidesga@gmail.com>
2019-07-03 14:20:35 -0600

    to pull

--- client/src/components/teacher/QuestionForm.js
+++ client/src/components/teacher/QuestionForm.js
@@ -12,7 +12,7 @@ const QuestionForm = (props) => {
     axios.post(`/api/quizzes/${props.match.params.id}/questions`,  { name, correct_answer: correctAnswer, } )
     .then( ques => {
       setName(ques.data)
-      history.push(`quizzes/${props.history.location.id}/questions`)
+        history.push(`quizzes/${props.history.location.id}/questions`)
       
     })
  }
